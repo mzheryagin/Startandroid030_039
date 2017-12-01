5a42b67390046de7256ccaaf177c8789b02fd04a
mzheryagin <zheryagin@gmail.com>
2017-12-01 17:58:08 +0300

    34 done

---
+++ P0331_SharedPreferences/src/main/java/ru/startandoid/sharedpreferences/MainActivity.java
@@ -0,0 +1,68 @@
+package ru.startandoid.sharedpreferences;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+  EditText etText;
+  Button btnSave;
+  Button btnLoad;
+
+  SharedPreferences sPref;
+
+  final String SAVED_TEXT = "saved_text";
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    etText = (EditText)findViewById(R.id.etText);
+    btnSave = (Button) findViewById(R.id.btnSave);
+    btnLoad = (Button)findViewById(R.id.btnLoad);
+
+    btnSave.setOnClickListener(this);
+    btnLoad.setOnClickListener(this);
+
+    loadText();
+  }
+
+  @Override
+  public void onClick(View view) {
+    switch (view.getId()){
+      case R.id.btnSave:
+        saveText();
+        break;
+      case R.id.btnLoad:
+        loadText();
+        break;
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    saveText();
+  }
+
+  void saveText(){
+    sPref = getPreferences(MODE_PRIVATE);
+    SharedPreferences.Editor ed = sPref.edit();
+    ed.putString(SAVED_TEXT, etText.getText().toString());
+    ed.commit();
+    Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+  }
+
+  void loadText(){
+    sPref = getPreferences(MODE_PRIVATE);
+    String savedText = sPref.getString(SAVED_TEXT, "");
+    etText.setText(savedText);
+    Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+  }
+}
---
+++ P0341_SimpleSQLite/src/main/java/ru/startandoid/simplesqlite/MainActivity.java
@@ -0,0 +1,120 @@
+package ru.startandoid.simplesqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+  final String LOG_TAG = "MyLogs";
+  Button btnAdd, btnRead, btnClear;
+  EditText etName, etEmail;
+  DBHelper dbHelper;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    btnAdd = (Button)findViewById(R.id.btnAdd);
+    btnAdd.setOnClickListener(this);
+    btnRead = (Button)findViewById(R.id.btnRead);
+    btnRead.setOnClickListener(this);
+    btnClear = (Button)findViewById(R.id.btnClear);
+    btnClear.setOnClickListener(this);
+    etName = (EditText)findViewById(R.id.etName);
+    etEmail = (EditText)findViewById(R.id.etEmail);
+
+    dbHelper = new DBHelper(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    //Создаем объект для данных
+    ContentValues cv = new ContentValues();
+
+    String name = etName.getText().toString();
+    String email = etEmail.getText().toString();
+
+    SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+    switch (view.getId()){
+      case R.id.btnAdd:
+        Log.d(LOG_TAG, "--- Insert in mytable: ---");
+
+        //Подготовим данные в виде пар: наименование столбца - значение
+        cv.put("name", name);
+        cv.put("email", email);
+
+        //Вставляем запись и получаем ее ID
+        long rowID = db.insert("mytable", null, cv);
+        Log.d(LOG_TAG, "row inserted ID = " + rowID);
+        break;
+      case R.id.btnRead:
+        // делаем запрос всех данных из таблицы mytable, получаем Cursor
+        Cursor c = db.query("mytable",
+            null,null,null,null,null,null);
+
+        // ставим позицию курсора на первую строку выборки
+        // если в выборке нет строк, вернется false
+        if(c.moveToFirst()){
+          //Определяем номера столбцов по имени в выборке
+          int idColIndex = c.getColumnIndex("id");
+          int nameColIndex = c.getColumnIndex("name");
+          int emailColIndex = c.getColumnIndex("email");
+
+          do{
+            //получаем значения по номерам и пишем в лог
+            Log.d(LOG_TAG,
+                "ID = " + c.getInt(idColIndex) +
+                    ", name = " + c.getString(nameColIndex) +
+                    ", email = " + c.getString(emailColIndex));
+
+            // переход на следующую строку
+            // а если следующей нет (текущая - последняя), то false - выходим из цикла
+          }while (c.moveToNext());
+
+        }else {
+          Log.d(LOG_TAG, "0 rows");
+          c.close();
+        }
+        break;
+      case R.id.btnClear:
+        Log.d(LOG_TAG, "--- Clear mytable: ---");
+        //удаляем все записи
+        int clearCount = db.delete("mytable", null,null );
+        Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+        break;
+    }
+    //Закрываем подключение к БД
+    dbHelper.close();
+  }
+
+  class DBHelper extends SQLiteOpenHelper{
+    public DBHelper(Context context){
+      super(context, "MyDB", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+      Log.d(LOG_TAG, "--- onCreate database ---");
+      db.execSQL("create table mytable ("
+          + "id integer primary key autoincrement,"
+          + "name text,"
+          + "email text" + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+  }
+
+}
+
