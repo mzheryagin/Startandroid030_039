9f8e431d59fba40533760c89229c370d1613657a
mzheryagin <zheryagin@gmail.com>
2017-12-21 17:58:03 +0300

    39 in progress

--- P0391_SQLiteOnUpgradeDB/src/main/java/ru/startandoid/sqliteonupgradedb/MainActivity.java
+++ P0391_SQLiteOnUpgradeDB/src/main/java/ru/startandoid/sqliteonupgradedb/MainActivity.java
@@ -70,27 +70,96 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-      Log.d(LOG_TAG, "--- onCreate database ---");
+      Log.d(LOG_TAG, " --- onCreate database --- ");
 
-      String[] people_name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис", "Костя", "Игорь" };
-      String[] people_positions = { "Программер", "Бухгалтер", "Программер", "Программер",
-          "Бухгалтер", "Директор", "Программер", "Охранник" };
+      String[] people_name = { "Иван", "Марья", "Петр", "Антон", "Даша",
+          "Борис", "Костя", "Игорь" };
+      int[] people_posid = { 2, 3, 2, 2, 3, 1, 2, 4 };
+
+      // данные для таблицы должностей
+      int[] position_id = { 1, 2, 3, 4 };
+      String[] position_name = { "Директор", "Программер", "Бухгалтер",
+          "Охранник" };
+      int[] position_salary = { 15000, 13000, 10000, 8000 };
 
       ContentValues cv = new ContentValues();
 
-      db.execSQL("CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, position TEXT);");
+      // создаем таблицу должностей
+      db.execSQL("create table position (" + "id integer primary key,"
+          + "name text, salary integer" + ");");
 
-      for(int i = 0; i < people_name.length; i++){
+      // заполняем ее
+      for (int i = 0; i < position_id.length; i++) {
+        cv.clear();
+        cv.put("id", position_id[i]);
+        cv.put("name", position_name[i]);
+        cv.put("salary", position_salary[i]);
+        db.insert("position", null, cv);
+      }
+
+      // создаем таблицу людей
+      db.execSQL("create table people ("
+          + "id integer primary key autoincrement,"
+          + "name text, posid integer);");
+
+      // заполняем ее
+      for (int i = 0; i < people_name.length; i++) {
         cv.clear();
         cv.put("name", people_name[i]);
-        cv.put("position", people_positions[i]);
+        cv.put("posid", people_posid[i]);
         db.insert("people", null, cv);
       }
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+      Log.d(LOG_TAG, "--- onUpgrade database from " + oldVersion + " to " + newVersion + " ---");
 
+      if(oldVersion == 1 && newVersion == 2){
+        ContentValues cv = new ContentValues();
+
+        //Данные для таблицы должностей
+        int[] positionId = { 1, 2, 3, 4 };
+        String[] positionName = { "Директор", "Программер", "Бухгалтер", "Охранник" };
+        int[] positionSalary = { 15000, 13000, 10000, 8000 };
+
+        db.beginTransaction();
+        try{
+          //создаем таблицу должностей
+          db.execSQL("CREATE TABLE position (id INTEGER PRIMARY KEY, name TEXT, salary INTEGER);");
+
+          for(int i = 0; i < positionId.length; i++){
+            cv.clear();
+            cv.put("id", positionId[i]);
+            cv.put("name", positionName[i]);
+            cv.put("salary", positionSalary[i]);
+            db.insert("position", null, cv);
+          }
+
+          db.execSQL("ALTER TABLE people ADD COLUMN posid INTEGER;");
+
+          for(int i = 0; i < positionId.length; i++){
+            cv.clear();
+            cv.put("posid", positionId[i]);
+            db.update("people", cv, "position = ?", new String[]{positionName[i]});
+          }
+
+          db.execSQL("CREATE TEMPORARY TABLE people_tmp (id INTEGER, name TEXT, position TEXT, posid INTEGER);");
+
+          db.execSQL("INSERT INTO people_tmp SELECT id, name, position, posid FROM people;");
+          db.execSQL("DROP TABLE people;");
+
+          db.execSQL("CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, posid INTEGER);");
+
+          db.execSQL("INSERT INTO people SELECT id, name, posid FROM people_tmp;");
+          db.execSQL("DROP TABLE people_tmp;");
+
+          db.setTransactionSuccessful();
+        }finally {
+          db.endTransaction();
+        }
+
+      }
     }
   }
 }
